720adc1129fafbf094955092850c0874b66467da
Rasyid <rasshit.dsgn@gmail.com>
2023-04-29 04:24:23 +0700

    3/11. Adding Bsic CSS Styling.mp4

--- my-code/react-complete-guide/src/components/ExpenseItem.js
+++ my-code/react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem(){
-    return <h2>Expense Item</h2>
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>April 29th 2023</div>
+      <div className="expense-item__decription">
+        <h2>Electrical Fee</h2>
+        <div className="expense-item__price">$50</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
