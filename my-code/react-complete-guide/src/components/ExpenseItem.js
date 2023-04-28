ad809158f2a82145404e73fb0a24d4106f31bdd9
Rasyid <rasshit.dsgn@gmail.com>
2023-04-29 04:41:45 +0700

    3/12. Outputing Dynamic Data & ...mp4

--- my-code/react-complete-guide/src/components/ExpenseItem.js
+++ my-code/react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 3, 29);
+    const expenseTitle = 'Electriacal Fee';
+    const expenseAmount = 50;
+
   return (
     <div className="expense-item">
-      <div>April 29th 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__decription">
-        <h2>Electrical Fee</h2>
-        <div className="expense-item__price">$50</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
