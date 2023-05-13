91bf29e6f37751fa08eef742e8c5364f71fd8555
Rasyid <rasshit.dsgn@gmail.com>
2023-05-13 08:50:20 +0700

    5/3.Using Statefull lists.mp4

--- my-code/react-complete-guide/src/App.js
+++ my-code/react-complete-guide/src/App.js
@@ -1,38 +1,44 @@
+import { useState } from 'react';
+
+import './App.css'
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Electric Fee",
+    amount: 100,
+    date: new Date(2021, 3, 29),
+  },
+  {
+    id: "e2",
+    title: "Internet Fee",
+    amount: 10,
+    date: new Date(2020, 3, 29),
+  },
+  {
+    id: "e3",
+    title: "Water Fee",
+    amount: 20,
+    date: new Date(2023, 3, 29),
+  },
+  {
+    id: "e4",
+    title: "Software Fee",
+    amount: 120,
+    date: new Date(2023, 3, 29),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Electric Fee",
-      amount: 100,
-      date: new Date(2021, 3, 29),
-    },
-    {
-      id: "e2",
-      title: "Internet Fee",
-      amount: 10,
-      date: new Date(2020, 3, 29),
-    },
-    {
-      id: "e3",
-      title: "Water Fee",
-      amount: 20,
-      date: new Date(2023, 3, 29),
-    },
-    {
-      id: "e4",
-      title: "Software Fee",
-      amount: 120,
-      date: new Date(2023, 3, 29),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   //this is store data newExpense
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses)=>{
+      return [expense, ...prevExpenses];
+    })
   }
 
   return (
