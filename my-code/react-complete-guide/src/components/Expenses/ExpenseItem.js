e1b6025f88687859a270cb65db48dc67014d0dc4
Rasyid <rasshit.dsgn@gmail.com>
2023-05-13 14:01:36 +0700

    5/Time to practice working with Lists.mp4

--- my-code/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ my-code/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -24,7 +24,7 @@ function ExpenseItem(props) {
         <div className="expense-item__price">${props.amount}</div>
       </div>
 
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
