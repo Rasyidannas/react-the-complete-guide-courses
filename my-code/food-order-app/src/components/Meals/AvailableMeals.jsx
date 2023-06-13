e5f4ad1fba8b54f7d139ce1f3d24ce1a881d38bc
Rasyid <rasshit.dsgn@gmail.com>
2023-06-13 17:32:45 +0700

    11/6. Adding individual Meal Items &....mp4

--- my-code/food-order-app/src/components/Meals/AvailableMeals.jsx
+++ my-code/food-order-app/src/components/Meals/AvailableMeals.jsx
@@ -1,3 +1,5 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/Mealitem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -28,13 +30,22 @@ const DUMMY_MEALS = [
 ];
 
 const AvalaibleMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
-    return <section className={classes.meals}>
-        <ul>
-            {mealsList}
-        </ul>
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
+  );
 };
 
 export default AvalaibleMeals;
