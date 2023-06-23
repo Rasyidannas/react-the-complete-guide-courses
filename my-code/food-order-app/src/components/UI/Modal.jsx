75eac0b97e1d879ee4a266993544bfe96d702c08
Rasyid <rasshit.dsgn@gmail.com>
2023-06-23 08:49:49 +0700

    11/ 11 - 12. adding a cart context.mp4

--- my-code/food-order-app/src/components/UI/Modal.jsx
+++ my-code/food-order-app/src/components/UI/Modal.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop}></div>
+    return <div className={classes.backdrop} onClick={props.onClose}></div>
 };
 
 const ModalOverlay = (props) => {
@@ -17,7 +17,7 @@ const portalElement = document.getElementById('overlays')
 const Modal = (props) => {
   return <Fragment>
     {/* this is protal for put element free like we want */}
-    {ReactDOM.createPortal(<Backdrop />, portalElement)}
+    {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
     {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
   </Fragment>;
 };
