46cdfad27e02a35316068b4cf10d2c899adc65f1
Rasyid <rasshit.dsgn@gmail.com>
2023-07-04 06:06:13 +0700

    11/19. Using the useEffect hook.mp4

--- my-code/food-order-app/src/components/Layout/HeaderCartButton.jsx
+++ my-code/food-order-app/src/components/Layout/HeaderCartButton.jsx
@@ -1,18 +1,44 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   //useContext for CartContext changed
   const cartCtx = useContext(CartContext);
+  
+  const {items} = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    
+    setBtnIsHighlighted(true);
+    
+    const timer = setTimeout(()=> {
+      setBtnIsHighlighted(false);
+    }, 300)
+    
+    //this will auto run
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
