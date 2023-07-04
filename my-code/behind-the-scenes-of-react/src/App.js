b5609fa75346f9e69b236b08db79e72b75c0ec82
Rasyid <rasshit.dsgn@gmail.com>
2023-07-04 11:35:47 +0700

    12/6. Preventing function ...mp4

--- my-code/behind-the-scenes-of-react/src/App.js
+++ my-code/behind-the-scenes-of-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "./components/UI/Button/Button";
 
 import "./App.css";
@@ -7,16 +7,17 @@ import DemoOutput from "./components/Demo/DemoOutput";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph(prevShowParagraph => !prevShowParagraph);
-  }
+  //this is for avoid function re-evaluated
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }, []);
 
-  console.log("App Running")
+  console.log("App Running");
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false}/>
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
