08ee970f0288e9aad24867ba2139c31271e005ee
Rasyid <rasshit.dsgn@gmail.com>
2023-07-09 03:59:15 +0700

    12/7. useCallback() and its....mp4

--- my-code/behind-the-scenes-of-react/src/App.js
+++ my-code/behind-the-scenes-of-react/src/App.js
@@ -6,18 +6,26 @@ import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  //this is for avoid function re-evaluated
+  //this is for avoid function re-evaluated and this will be execute when allowToggle change or Allow Toggling clicked for change allowToggle
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    if(allowToggle){
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true)
+  };
 
   console.log("App Running");
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
