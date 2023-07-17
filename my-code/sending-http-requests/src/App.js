dd8cfdc0436d56326fffc8e14eaf0d580fb51231
Rasyid <rasshit.dsgn@gmail.com>
2023-07-17 09:18:19 +0700

    14/11. Sending a POST Requesr.mp4

--- my-code/sending-http-requests/src/App.js
+++ my-code/sending-http-requests/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
-import AddMovie from './components/AddMovie';
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -9,13 +9,15 @@ function App() {
   const [isLoding, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  //fetch API
+  //fetch API for get Data
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
-      const response = await fetch("https://reasct-http-39dc5-default-rtdb.firebaseio.com/movies.json");
+      const response = await fetch(
+        "https://react-http-1a345-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+      );
 
       //this is for response not successfull
       if (!response.ok) {
@@ -24,16 +26,17 @@ function App() {
 
       const data = await response.json();
 
-      //this is for change re-structured data and response successfull
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -45,8 +48,21 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  //fetch API for send Data
   async function addMovieHandler(movie) {
-    console.log();
+    const response = await fetch(
+      "https://react-http-1a345-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
