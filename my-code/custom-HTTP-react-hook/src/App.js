845922ab1fa720248165741331c73ae2749996eb
Rasyid <rasshit.dsgn@gmail.com>
2023-07-21 11:06:04 +0700

    15/9. Adjusting the custom hook logic.mp4

--- my-code/custom-HTTP-react-hook/src/App.js
+++ my-code/custom-HTTP-react-hook/src/App.js
@@ -7,29 +7,28 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (taskObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
   //this is custom hook http
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    { url: "https://react-http-1a345-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json" },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
+  //this is avoid infinity looping
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (taskObj) => {
+      const loadedTasks = [];
+  
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+  
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-1a345-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- my-code/custom-HTTP-react-hook/src/hooks/use-http.js
+++ my-code/custom-HTTP-react-hook/src/hooks/use-http.js
@@ -1,38 +1,38 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
-const useHttp = (requstConfig, applyData) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const sendRequest = async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(
-          requstConfig.url, {
-            method: requstConfig.method ? requstConfig.method : 'GET',
-            headers: requstConfig.headers ? requstConfig.headers : {},
-            body: JSON.stringify(requstConfig.body) ? JSON.stringify(requstConfig.body) : null
-          }
-        );
-  
-        if (!response.ok) {
-          throw new Error('Request failed!');
-        }
-  
-        const data = await response.json();
-        applyData(data)
-      } catch (err) {
-        setError(err.message || 'Something went wrong!');
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requstConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requstConfig.url, {
+        method: requstConfig.method ? requstConfig.method : "GET",
+        headers: requstConfig.headers ? requstConfig.headers : {},
+        body: JSON.stringify(requstConfig.body)
+          ? JSON.stringify(requstConfig.body)
+          : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
       }
-      setIsLoading(false);
-    };
 
-    return {
-        isLoading, 
-        error, 
-        sendRequest
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
     }
-}
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
 
 export default useHttp;
