83e7c6ecf235417edc2332f0e8b950cc9c556508
Rasyid <rasshit.dsgn@gmail.com>
2023-07-25 11:25:15 +0700

    16/7. Handling the was touched state.mp4

--- my-code/working-with-forms-user-input/src/components/SimpleInput.jsx
+++ my-code/working-with-forms-user-input/src/components/SimpleInput.jsx
@@ -4,7 +4,7 @@ const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   useEffect(() => {
     if (enteredNameIsValid) {
@@ -15,12 +15,11 @@ const SimpleInput = (props) => {
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
-  
-  
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-    
-    setEnteredNameTouched(true)
+
+    setEnteredNameTouched(true);
 
     if (enteredName.trim() == "") {
       setEnteredNameIsValid(false);
@@ -32,7 +31,7 @@ const SimpleInput = (props) => {
 
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
-    
+
     setEnteredName("");
   };
 
@@ -42,7 +41,7 @@ const SimpleInput = (props) => {
   const nameInputClasses = nameInputIsInvalid
     ? "form-control invalid"
     : "form-control";
-  
+
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
