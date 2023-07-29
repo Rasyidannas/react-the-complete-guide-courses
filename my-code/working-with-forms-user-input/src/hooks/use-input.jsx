55320ff0565610c811b34d0e07714d0ab6b9fa9b
rasyid annas <rasshit.dsgn@gmail.com>
2023-07-29 12:18:04 +0700

    16/11 - 12 adding a custom input hook.mp4

---
+++ my-code/working-with-forms-user-input/src/hooks/use-input.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  //this is for enteredValue not empty
+  const valueIsValid = validateValue(enteredValue);
+  //this is 2 condition (valueIsValid = false && isTouched = true)
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset
+  };
+};
+
+export default useInput;
