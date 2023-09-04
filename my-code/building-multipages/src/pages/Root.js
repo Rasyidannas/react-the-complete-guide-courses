54fbbe91566a1129a1503c1725ad872c15e2c828
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-04 12:38:49 +0700

    20/9. Showing Error Pages with errorElement.mp4

--- my-code/building-multipages/src/pages/Root.js
+++ my-code/building-multipages/src/pages/Root.js
@@ -1,12 +1,11 @@
 import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
-import classes from "./Root.module.css";
 
 function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <main className={classes.comtemt}>
+      <main>
         <Outlet />
       </main>
     </>
