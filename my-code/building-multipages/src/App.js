8398587c8bbfb2f6d24cb3ffb5fa492430db64c1
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-04 10:57:56 +0700

    20/7. Navigating between Pages with Links.mp4

--- my-code/building-multipages/src/App.js
+++ my-code/building-multipages/src/App.js
@@ -1,25 +1,25 @@
 import {
   createBrowserRouter,
-  createRoutesFromElements,
-  Route,
+  // createRoutesFromElements,
+  // Route,
   RouterProvider,
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductPage from "./pages/Products";
 
-const routerDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductPage />} />
-  </Route>
-)
+// const routerDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductPage />} />
+//   </Route>
+// )
 
-const router = createBrowserRouter(routerDefinitions);
+// const router = createBrowserRouter(routerDefinitions);
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductPage /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductPage /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
