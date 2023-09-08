d8411040fcaec3d6348d3f787841f349235d53b1
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-08 08:28:34 +0700

    20/13. Adding Links for Dynamic Routes.mp4

--- my-code/building-multipages/src/App.js
+++ my-code/building-multipages/src/App.js
@@ -8,6 +8,7 @@ import HomePage from "./pages/Home";
 import ProductPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routerDefinitions = createRoutesFromElements(
 //   <Route>
@@ -26,6 +27,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductPage /> },
+      { path: "/product/:productId", element: <ProductDetailPage /> }
     ],
   },
 ]);
