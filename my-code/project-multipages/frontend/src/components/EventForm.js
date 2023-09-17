b52c8ffc5290b3a0c33966b2cabcfce1493f2bee
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-17 13:51:08 +0700

    20/33. Working with action() Funtions.mp4

--- my-code/project-multipages/frontend/src/components/EventForm.js
+++ my-code/project-multipages/frontend/src/components/EventForm.js
@@ -1,15 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Form } from 'react-router-dom';
 
-import classes from "./EventForm.module.css";
+import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
   function cancelHandler() {
-    navigate("..");
+    navigate('..');
   }
 
   return (
-    <form className={classes.form}>
+    <Form method='post' className={classes.form}>
       <p>
         <label htmlFor="title">Title</label>
         <input
@@ -17,7 +17,7 @@ function EventForm({ method, event }) {
           type="text"
           name="title"
           required
-          defaultValue={event ? event.title : ""}
+          defaultValue={event ? event.title : ''}
         />
       </p>
       <p>
@@ -27,7 +27,7 @@ function EventForm({ method, event }) {
           type="url"
           name="image"
           required
-          defaultValue={event ? event.image : ""}
+          defaultValue={event ? event.image : ''}
         />
       </p>
       <p>
@@ -37,7 +37,7 @@ function EventForm({ method, event }) {
           type="date"
           name="date"
           required
-          defaultValue={event ? event.date : ""}
+          defaultValue={event ? event.date : ''}
         />
       </p>
       <p>
@@ -47,7 +47,7 @@ function EventForm({ method, event }) {
           name="description"
           rows="5"
           required
-          defaultValue={event ? event.description : ""}
+          defaultValue={event ? event.description : ''}
         />
       </p>
       <div className={classes.actions}>
@@ -56,7 +56,7 @@ function EventForm({ method, event }) {
         </button>
         <button>Save</button>
       </div>
-    </form>
+    </Form>
   );
 }
 
