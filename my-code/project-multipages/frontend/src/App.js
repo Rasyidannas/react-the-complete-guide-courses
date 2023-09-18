88fc403d19b6fd0991149ade80b37930eb73a345
rasyid annas <rasshit.dsgn@gmail.com>
2023-09-18 10:50:02 +0700

    20/36. Reusing Actions via Request Methods.mp4

--- my-code/project-multipages/frontend/src/App.js
+++ my-code/project-multipages/frontend/src/App.js
@@ -26,11 +26,12 @@ import EventDetailPage, {
   loader as eventDetailLoader,
   action as deletEventAction,
 } from "./pages/EventDetail";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
+import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventRootlayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
+import { action as manipulateEventAction } from "./components/EventForm"
 
 const router = createBrowserRouter([
   {
@@ -58,10 +59,10 @@ const router = createBrowserRouter([
                 element: <EventDetailPage />,
                 action: deletEventAction,
               },
-              { path: "edit", element: <EditEventPage /> },
+              { path: "edit", element: <EditEventPage />, action: manipulateEventAction },
             ],
           },
-          { path: "new", element: <NewEventPage />, action: newEventAction },
+          { path: "new", element: <NewEventPage />, action: manipulateEventAction },
         ],
       },
     ],
