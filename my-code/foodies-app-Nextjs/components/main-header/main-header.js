1f2bc00e5067afedef3fad5a14cc2cf510ac7a01
rasyidannas <rasshit.dsgn@gmail.com>
2025-06-19 11:28:53 +0000

    26/447. Using Client Components Efficiently.mp4

--- my-code/foodies-app-Nextjs/components/main-header/main-header.js
+++ my-code/foodies-app-Nextjs/components/main-header/main-header.js
@@ -1,11 +1,14 @@
+
 import Link from "next/link";
 import Image from 'next/image';
 
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background.js"
+import NavLink from "./nav-link.js"
 
 export default function MainHeader() {
+
   return (
     <>
       <MainHeaderBackground />
@@ -18,10 +21,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browser Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foddies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
